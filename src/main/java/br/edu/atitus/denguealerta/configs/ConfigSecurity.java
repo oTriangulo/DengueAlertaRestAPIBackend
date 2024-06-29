b891d7dc9970e044b3e48e558c8268399303a7ef
oTriangulo <guusswb@gmail.com>
2024-06-28 21:59:34 -0300

    Update ConfigSecurity.java

--- src/main/java/br/edu/atitus/denguealerta/configs/ConfigSecurity.java
+++ src/main/java/br/edu/atitus/denguealerta/configs/ConfigSecurity.java
@@ -7,11 +7,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.edu.atitus.denguealerta.components.AuthToken;
 
 @Configuration
 public class ConfigSecurity {
+	
+	@Bean
+    WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("");
+			}
+		};
+	}
 	
 	@Bean
 	SecurityFilterChain getSecurity(HttpSecurity http, AuthToken authToken) throws Exception {
